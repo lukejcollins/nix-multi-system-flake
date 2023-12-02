6d5764f0a33e076dc845f58cf6c943054087537f
Luke Collins <luke.collins@live.co.uk>
2023-12-02 23:15:01 +0000

    Add ubersicht

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,10 +1,30 @@
 { config, pkgs, ... }:
 
+let
+  uebersicht = pkgs.stdenv.mkDerivation {
+    name = "Uebersicht-1.6.77";
+    buildInputs = [ pkgs.unzip pkgs.glibcLocales ];
+    src = pkgs.fetchurl {
+      url = "https://tracesof.net/uebersicht/releases/Uebersicht-1.6.77.app.zip";
+      sha256 = "sha256-jxGxY1YYCMylJs7kkUCyIczNo7u2n1qcoL2KdQ58h70="; # Placeholder SHA
+    };
+
+    unpackPhase = ''
+      export LANG=en_US.UTF-8
+      export LC_ALL=en_US.UTF-8
+      unzip $src
+    '';
+
+    installPhase = ''
+      mkdir -p $out/Applications
+      cp -R "Übersicht.app" $out/Applications/
+    '';
+  };
+in
 {
-  # List packages installed in system profile.
-  environment.systemPackages = with pkgs; [ 
+  environment.systemPackages = with pkgs; [
     vim git gh alacritty wget docker nodejs python3 python3Packages.pip vscode shellcheck
-    shfmt statix nixpkgs-fmt postgresql docker-compose tailscale
+    shfmt statix nixpkgs-fmt postgresql docker-compose tailscale uebersicht
   ];
 
   nixpkgs.config.allowUnfree = true;
@@ -15,12 +35,12 @@
       enable = true;
     };
 
-    yabai = { 
+    yabai = {
       enable = true;
       package = pkgs.yabai;
       extraConfig = "/Users/luke.collins/.config/yabai/yabairc";
     };
-    
+
     skhd = {
      enable = true;
      package = pkgs.skhd;
