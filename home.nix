5edc1ee2699b3148b0913cc20e088c0a18fc9f09
Luke Collins <luke.collins@live.co.uk>
2023-12-03 14:26:29 +0000

    Add simplebar and setup simplebar config

--- home.nix
+++ home.nix
@@ -1,13 +1,27 @@
+{ pkgs, lib, ... }:
+
+let
+  simpleBarWidget = pkgs.fetchFromGitHub {
+    owner = "lukejcollins";
+    repo = "simple-bar";
+    rev = "b275b7499861590437faada2aff1a75e2a183945";
+    sha256 = "sha256-IlURi4lcLUHdzH/N3wN06oFMGJstFSqDjgRHCp3h6WQ="; # Replace with the actual SHA
+  };
+
+in
 {
   home = {
     sessionVariables = {
       EDITOR = "vim";
     };
-    # Enable dark mode
+
     stateVersion = "23.11"; # Make sure this matches the Nixpkgs version you are using
+
     file.".config/yabai/yabairc".source = ./dotfiles/yabai/yabairc;
     file.".skhdrc".source = ./dotfiles/.skhdrc;
-  };
+    file.".simplebarrc".source = ./dotfiles/.simplebarrc;
 
+    file."Library/Application Support/Übersicht/widgets/simple-bar".source = simpleBarWidget;
+  };
   # Note: Zsh is not enabled here as it's managed by Nix Darwin instead
 }
