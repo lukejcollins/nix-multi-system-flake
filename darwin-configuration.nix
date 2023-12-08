769f644a51a496ca4812d24d8e4de2b4e9ef5759
Luke Collins <luke.collins@live.co.uk>
2023-12-08 16:47:45 +0000

    Add Raycast and make settings a float

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -31,7 +31,7 @@ in
   environment.systemPackages = with pkgs; [
     vim git gh alacritty wget docker nodejs python3 python3Packages.pip vscode shellcheck
     shfmt statix nixpkgs-fmt postgresql docker-compose tailscale uebersicht gcc direnv tflint
-    hadolint neofetch colima
+    hadolint neofetch colima raycast
     # Install emacs with packages
     (emacsWithPackagesFromUsePackage {
       config = ./emacs/init.el;
