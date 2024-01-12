ceaf7b463fcc7885ea30630f47d1435aeab5300d
Luke Collins <luke.collins@live.co.uk>
2024-01-12 13:34:53 +0000

    Switch to nixpkgs unstable and hm master and update flake

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Darwin personal setup flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.inputs.home-manager.follows = "home-manager";
   };
