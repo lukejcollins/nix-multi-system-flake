661673713f9aab3401560c1a1b26fc8cce986225
Luke Collins <luke.collins@live.co.uk>
2024-08-03 11:03:11 +0100

    Add terraform

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@ in
     shfmt postgresql docker-compose tailscale gcc direnv neofetch pyright
     nil bash-language-server dockerfile-language-server-nodejs terraform-ls
     clippy awscli2 typst yarn fzf spotify yaml-language-server act jq kubectl minikube
-    aws-nuke tre-command fzf bat eza
+    aws-nuke tre-command fzf bat eza terraform
     # Install Emacs with packages
     (emacsWithPackagesFromUsePackage {
       config = ./emacs/init.el;
