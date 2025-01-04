076d3c91f05e64f13f312003e38406bc8c7c7265
Luke Collins <luke.collins@live.co.uk>
2025-01-04 11:10:48 +0000

    Add emacs to WSL

--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -48,3 +48,6 @@ export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH
 
 # direnv zsh hook
 eval "$(direnv hook zsh)"
+
+# emacs alias
+alias emacs='~/bin/fullscreen.sh emacs'
--- wsl/home.nix
+++ wsl/home.nix
@@ -1,14 +1,33 @@
 { pkgs, lib, ... }:
 
 let
-  # Placeholder for future variables or configurations
+  emacsPackage = if pkgs.stdenv.hostPlatform.system == "aarch64-darwin" then
+    pkgs.emacs29-macport
+  else
+    pkgs.emacs29;
 in
 {
-  programs.zsh = {
-    enable = true;
+  programs = {
+    zsh.enable = true;
+    emacs = {
+      enable = true;
+      package = emacsPackage;
+      extraPackages = epkgs: with epkgs; [
+        use-package terraform-mode flycheck flycheck-inline dockerfile-mode nix-mode
+        treemacs markdown-mode treemacs-all-the-icons modus-themes helm vterm grip-mode
+        dash s editorconfig autothemer rust-mode lsp-mode dashboard direnv projectile
+        nerd-icons doom-modeline company catppuccin-theme yaml-mode csv-mode
+        codeium web-mode lsp-ui treemacs-nerd-icons nerd-icons
+      ];
+    };
   };
 
-  home.packages = with pkgs; [
-    zsh direnv gh zellij home-manager iperf3
-  ];
+  fonts.fontconfig.enable = true;  
+
+  home = {
+    packages = with pkgs; [ zsh direnv gh zellij home-manager iperf3 
+                            nerd-fonts.symbols-only xdotool neofetch ];
+    file.".emacs.d/init.el".source = ./emacs/init.el;
+    file."bin/fullscreen.sh".source = ./scripts/fullscreen.sh;
+  };
 }
