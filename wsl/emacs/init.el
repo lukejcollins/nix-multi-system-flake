8a8c7729b7be2a271f618fcc80250e1bb7b1ca11
lukecollins <lukecollins@debian>
2025-01-18 09:51:41 +0000

    Setup qemu build

--- wsl/emacs/init.el
+++ wsl/emacs/init.el
@@ -225,27 +225,6 @@
         ;; company-frontends '(company-pseudo-tooltip-frontend company-preview-frontend)
         ))
 
-;; Configure codeium
-(use-package codeium
-    :ensure t
-    :init
-    (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
-    :config
-    (setq use-dialog-box nil) ;; do not use popup boxes
-    (setq codeium-mode-line-enable
-        (lambda (api) (not (memq api '(CancelRequest Heartbeat AcceptCompletion)))))
-    (add-to-list 'mode-line-format '(:eval (car-safe codeium-mode-line)) t)
-    (setq codeium-api-enabled
-        (lambda (api)
-            (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion))))
-    (defun my-codeium/document/text ()
-        (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (min (+ (point) 1000) (point-max))))
-    (defun my-codeium/document/cursor_offset ()
-        (codeium-utf8-byte-length
-            (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
-    (setq codeium/document/text 'my-codeium/document/text)
-    (setq codeium/document/cursor_offset 'my-codeium/document/cursor_offset))
-
 ;; Treemacs configuration
 (use-package treemacs
   :ensure t
